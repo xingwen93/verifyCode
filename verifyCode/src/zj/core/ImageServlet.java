f04d4b2009bb985b009dd001f7c4437f2a752e9d
zjinger <892295834@qq.com>
2016-05-26 17:23:17 +0800

    新增ImageServlet

---
+++ verifyCode/src/zj/core/ImageServlet.java
@@ -0,0 +1,12 @@
+package zj.core;
+/**
+ * @author ZhangJing
+ * @version 创建时间：2016年5月26日 下午5:22:28
+ * 类说明
+ */
+public class ImageServlet extends BaseServlet{
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+}
